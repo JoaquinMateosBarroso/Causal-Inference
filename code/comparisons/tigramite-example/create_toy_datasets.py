b61abffdaab74e2256ffde47b78673dd4b1272cd
Joaquín Mateos Barroso <jmateosbarroso@gmail.com>
2025-01-12 14:28:01 +0100

    created full function to test causal discovery algorithms

--- code/comparisons/tigramite-example/create_toy_datasets.py
+++ code/comparisons/tigramite-example/create_toy_datasets.py
@@ -11,14 +11,26 @@ def get_parents_dict(causal_process):
             parents_dict[key].append(causal_process[key][i][0])
     return parents_dict
 
-def generate_toy_data(name):
+def generate_toy_data(name, T=100, N=10, L=10, max_lag=3, dependency_funcs=['nonlinear']):
+    """
+    Generate a toy dataset with a causal process and time series data
+    :param name: Name of the dataset
+    :param T: Number of time points
+    :param N: Number of variables
+    :param L: Number of cross-links between two different variables.
+    :param max_lag: Maximum lag of the causal process
+    :param dependency_funcs: List of dependency functions (in {'linear', 'nonlinear'})
+    """
     # Generate random causal process
-    causal_process = generate_structural_causal_process(N=20, L=5, max_lag=3, dependency_funcs=['nonlinear'], seed=0)[0]
+    causal_process = generate_structural_causal_process(N=N,
+                                                        L=L,
+                                                        max_lag=max_lag,
+                                                        dependency_funcs=dependency_funcs,
+                                                        seed=0)[0]
     actual_dict = get_parents_dict(causal_process)
 
     # Generate time series data from the causal process
-    time_series = structural_causal_process(causal_process, T=100)[0]
-    
+    time_series = structural_causal_process(causal_process, T=T)[0]
     
     # Save the causal process to a txt file
     with open(f'toy_data/causal_process{name}.txt', 'w') as f:
--- code/comparisons/tigramite-example/functions_test_toy_data.py
+++ code/comparisons/tigramite-example/functions_test_toy_data.py
@@ -14,7 +14,13 @@ from tigramite.pcmci import PCMCI
 from tigramite.lpcmci import LPCMCI
 
 from tigramite.independence_tests.parcorr import ParCorr
+import time
 
+
+# %%
+'''
+    EVALUATION METRICS
+'''
 def get_precision(ground_truth_parents: dict, predicted_parents: dict):
     # Precision = TP / (TP + FP)
     true_positives = 0
@@ -35,7 +41,29 @@ def get_recall(ground_truth_parents: dict, predicted_parents: dict):
     
     return true_positives / ground_truth_positives if ground_truth_positives != 0 else 0
 
-def test_toy_data(name, parents_extractor):
+def get_f1(ground_truth_parents: dict, predicted_parents: dict):
+    precision = get_precision(ground_truth_parents, predicted_parents)
+    recall = get_recall(ground_truth_parents, predicted_parents)
+    
+    return 2 * precision * recall / (precision + recall) if precision + recall != 0 else 0
+
+def get_false_positive_ration(ground_truth_parents: dict, predicted_parents: dict):
+    # FPR = FP / (FP + TN)
+    false_positives = 0
+    for effect, causes in predicted_parents.items():
+        false_positives += len([cause for cause in causes if cause not in ground_truth_parents.get(effect, [])])
+    
+    true_negatives = 0
+    for effect, causes in ground_truth_parents.items():
+        true_negatives += len([cause for cause in causes if cause not in predicted_parents.get(effect, [])])
+    
+    return false_positives / (false_positives + true_negatives) if false_positives != 0 else 0
+
+
+
+
+# %%
+def test_toy_data(name, parents_extractor, verbose=0):
     # Load causal process dictionary
     with open(f'toy_data/causal_process{name}.txt') as f:
         line = f.readline()
@@ -45,17 +73,28 @@ def test_toy_data(name, parents_extractor):
     dataframe = pp.DataFrame(df.values, var_names=df.columns)
     dataframe.values[0].shape
     
-    parents = parents_extractor(dataframe)
     
-    # Compare results
-    print(f'Predicted parents: {parents}')
-    print(f'Causal process: {causal_process}')
+    parents, time = parents_extractor(dataframe)
     
-    print(f'Precision: {get_precision(parents, causal_process)}')
-    print(f'Recall: {get_recall(parents, causal_process)}')
-    print(f'F1: {2 * get_precision(parents, causal_process) * get_recall(parents, causal_process) / (get_precision(parents, causal_process) + get_recall(parents, causal_process))}')
+    precision = get_precision(parents, causal_process)
+    recall = get_recall(parents, causal_process)
+    f1 = get_f1(parents, causal_process)
+    if verbose>0:
+        # Compare results
+        print(f'Predicted parents: {parents}')
+        print(f'Causal process: {causal_process}')
+        
+        print(f'Precision: {precision}')
+        print(f'Recall: {recall}')
+        print(f'F1: {f1}')
+    
+    return {'precision': precision, 'recall': recall, 'f1': f1, 'time': time}
+    
 
 def extract_parents_pcmci(dataframe):
+    '''
+    Returns the parents dict and the time that took to run the algorithm
+    '''
     parcorr = ParCorr(significance='analytic')
     pcmci = PCMCI(
         dataframe=dataframe,
@@ -65,12 +104,19 @@ def extract_parents_pcmci(dataframe):
 
     tau_max = 3
     pc_alpha = None # Optimize in a list
+    
+    start_time = time.time()
     results = pcmci.run_pcmci(tau_min=1, tau_max=tau_max, pc_alpha=pc_alpha)
+    end_time = time.time()
+    execution_time = end_time - start_time
 
     parents = pcmci.return_parents_dict(graph=results['graph'], val_matrix=results['val_matrix'])
-    return parents
+    return parents, execution_time
 
 def extract_parents_pcmciplus(dataframe):
+    '''
+    Returns the parents dict and the time that took to run the algorithm
+    '''
     parcorr = ParCorr(significance='analytic')
     pcmci = PCMCI(
         dataframe=dataframe,
@@ -80,31 +126,45 @@ def extract_parents_pcmciplus(dataframe):
 
     tau_max = 3
     pc_alpha = None # Default value
+    
+    
+    start_time = time.time()
     results = pcmci.run_pcmciplus(tau_min=1, tau_max=tau_max, pc_alpha=pc_alpha)
+    end_time = time.time()
+    execution_time = end_time - start_time
 
+    
+    
     parents = pcmci.return_parents_dict(graph=results['graph'], val_matrix=results['val_matrix'])
-    return parents
+    return parents, execution_time
 
-def extract_parents_lpcmci(dataframe):
+def extract_parents_lpcmci(dataframe) -> tuple[dict, float]:
+    '''
+    Returns the parents dict and the time that took to run the algorithm
+    '''
     parcorr = ParCorr(significance='analytic')
     lpcmci = LPCMCI(
         dataframe=dataframe,
         cond_ind_test=parcorr,
         verbosity=0
     )
-
     
     tau_max = 3
     pc_alpha = 0.05 # Default value
-    results = lpcmci.run_lpcmci(tau_min=1, tau_max=tau_max, pc_alpha=pc_alpha)    
     
+    start_time = time.time()
+    lpcmci.run_lpcmci(tau_min=1, tau_max=tau_max, pc_alpha=pc_alpha)
+    end_time = time.time()
+    execution_time = end_time - start_time
+    
+    # Return parents dict manually
     parents = dict()
     for j in range(lpcmci.N):
         for ((i, lag_i), link) in lpcmci.graph_dict[j].items():
             if len(link) > 0 and (lag_i < 0 or i < j):
                 parents[j] = parents.get(j, []) + [(i, lag_i)]
     
-    return parents
+    return parents, execution_time
 
 
 if __name__ == '__main__':
