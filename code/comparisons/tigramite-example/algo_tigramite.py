49482be60a7d09108c38d053e21ca915d2494454
Joaquín Mateos Barroso <jmateosbarroso@gmail.com>
2025-01-27 21:33:56 +0100

    prepared benchmarkings

--- code/comparisons/tigramite-example/algo_tigramite.py
+++ code/comparisons/tigramite-example/algo_tigramite.py
@@ -9,76 +9,92 @@ from tigramite.lpcmci import LPCMCI
 from causalai.data.time_series import TimeSeriesData
 
 
+def convert_to_tigramite_format(data: TimeSeriesData) -> tigramite.data_processing.DataFrame:
+    '''
+    Convert the data to tigramite dataframe format
+    Note: It only works if there is only one data array in the data object
+    '''
+    names = data.var_names
+    data_arrays = data.data_arrays
+    dataframe = tigramite.data_processing.DataFrame(data_arrays[0], var_names=names)
+    
+    return dataframe
+
+def convert_to_causalai_format(parents_graph: dict[int, tuple[int, int]], names: list[str]) -> dict:
+    '''
+    Convert the graph to causalai format
+    '''
+    parents_getter = lambda parents: \
+            [(str(index), time_lag) for index, time_lag in parents]
+    
+    return {name: {'parents': parents_getter(parents)} for name, parents \
+                                             in zip(names, parents_graph.values())}
+
 class Extractor_PCMCI():
-    def __init__(self, data: TimeSeriesData):
+    def __init__(self, data: TimeSeriesData, cond_ind_test: str = 'parcorr'):
         '''
         :param data: this is a TabularData object and contains attributes likes data.data_arrays, which is a
             list of numpy array of shape (observations N, variables D).
         :type data: TabularData object
         '''
         self.data = data
-        names = data.var_names
-        data_arrays = data.data_arrays
-        print(data_arrays)
-        self.dataframe = tigramite.data_processing.DataFrame(data_arrays, var_names=names)
+        self.dataframe = convert_to_tigramite_format(data)
+        
+        self.cond_ind_test = {'parcorr': ParCorr(significance='analytic')}[cond_ind_test]
+        
+        self.pcmci = PCMCI(dataframe=self.dataframe,
+                           cond_ind_test=self.cond_ind_test)
+        
+
+    def run(self, **kargs):
+        '''
+        Get the parents graph
+        '''
+        tau_max = kargs['tau_max']
+        pc_alpha = kargs['pc_alpha']
+        
+        results = self.pcmci.run_pcmciplus(tau_min=1, tau_max=tau_max, pc_alpha=pc_alpha)
+        
+        parents_graph = self.pcmci.return_parents_dict(graph=results['graph'], val_matrix=results['val_matrix'])
+        
+        result = convert_to_causalai_format(parents_graph, self.data.var_names)
+        
+        return result
+
+class Extractor_LPCMCI():
+    def __init__(self, data: TimeSeriesData, cond_ind_test: str = 'parcorr'):
+        '''
+        :param data: this is a TabularData object and contains attributes likes data.data_arrays, which is a
+            list of numpy array of shape (observations N, variables D).
+        :type data: TabularData object
+        '''
+        self.data = data
+        self.dataframe = convert_to_tigramite_format(data)
+        
+        self.cond_ind_test = {'parcorr': ParCorr(significance='analytic')}[cond_ind_test]
+        
+        self.lpcmci = LPCMCI(
+            dataframe=self.dataframe,
+            cond_ind_test=self.cond_ind_test)
         
 
     def run(self, **kargs):
         tau_max = kargs['tau_max']
         pc_alpha = kargs['pc_alpha']
         
-        result = {name: {'parents': []} for name in self.data.var_names} # result must follow this format
-        # do something and compute parents
-        return result
+        self.lpcmci.run_lpcmci(tau_min=1, tau_max=tau_max, pc_alpha=pc_alpha)
 
-def extract_parents_pcmci(df):
-    '''
-    Returns the parents dict and the time that took to run the algorithm
-    '''
-    
-    parcorr = ParCorr(significance='analytic')
-    pcmci = PCMCI(
-        dataframe=dataframe,
-        cond_ind_test=parcorr,
-        verbosity=0
-    )
+        # Return parents dict manually
+        graph_parents = dict()
+        for j in range(self.lpcmci.N):
+            current_parents = []
+            for ((i, lag_i), link) in self.lpcmci.graph_dict[j].items():
+                if len(link) > 0 and (lag_i < 0 or i < j):
+                    current_parents.append((str(i), lag_i))
+            graph_parents[str(j)] = {'parents': current_parents}
 
-    tau_max = 3
-    pc_alpha = None # Optimize in a list
-    
-    start_time = time.time()
-    results = pcmci.run_pcmci(tau_min=1, tau_max=tau_max, pc_alpha=pc_alpha)
-    end_time = time.time()
-    execution_time = end_time - start_time
+        return graph_parents
 
-    parents = pcmci.return_parents_dict(graph=results['graph'], val_matrix=results['val_matrix'])
-    
-    return parents, execution_time
-
-def extract_parents_pcmciplus(dataframe):
-    '''
-    Returns the parents dict and the time that took to run the algorithm
-    '''
-    parcorr = ParCorr(significance='analytic')
-    pcmci = PCMCI(
-        dataframe=dataframe,
-        cond_ind_test=parcorr,
-        verbosity=0
-    )
-
-    tau_max = 3
-    pc_alpha = None # Default value
-    
-    
-    start_time = time.time()
-    results = pcmci.run_pcmciplus(tau_min=1, tau_max=tau_max, pc_alpha=pc_alpha)
-    end_time = time.time()
-    execution_time = end_time - start_time
-
-    
-    
-    parents = pcmci.return_parents_dict(graph=results['graph'], val_matrix=results['val_matrix'])
-    return parents, execution_time
 
 def extract_parents_lpcmci(dataframe) -> tuple[dict, float]:
     '''
--- code/comparisons/tigramite-example/benchmark_causalai.py
+++ code/comparisons/tigramite-example/benchmark_causalai.py
@@ -13,7 +13,7 @@ from causalai.models.time_series.var_lingam import VARLINGAM
 from functools import partial
 import numpy as np
 
-from algo_tigramite import Extractor_PCMCI
+from algo_tigramite import Extractor_LPCMCI, Extractor_PCMCI
 
 
 
@@ -22,27 +22,45 @@ from algo_tigramite import Extractor_PCMCI
 
 algo_dict = {
             'PCMCI': partial(Extractor_PCMCI),
+            'LPCMCI': partial(Extractor_LPCMCI),
             'PC-PartialCorr':partial(PC, CI_test=PartialCorrelation(), use_multiprocessing=False,
                                       prior_knowledge=None),
             'Granger':partial(Granger, use_multiprocessing=False, prior_knowledge=None),
-           'VARLINGAM':partial(VARLINGAM, use_multiprocessing=True, prior_knowledge=None)}
+            'VARLINGAM':partial(VARLINGAM, use_multiprocessing=True, prior_knowledge=None)}
 
 kargs_dict = {
-            'PCMCI': {'tau_max': 3, 'pc_alpha': None},
+            'PCMCI': {'tau_max': 3, 'pc_alpha': 0.01},
+            'LPCMCI': {'tau_max': 3, 'pc_alpha': 0.01},
             'PC-PartialCorr': {'max_condition_set_size': 4, 'pvalue_thres': 0.01, 'max_lag': 3},
             'Granger': {'pvalue_thres': 0.01, 'max_lag': 3},
-           'VARLINGAM': {'pvalue_thres': 0.01, 'max_lag': 3}}
+            'VARLINGAM': {'pvalue_thres': 0.01, 'max_lag': 3}}
 
 b = BenchmarkContinuousTimeSeries(algo_dict=algo_dict, kargs_dict=kargs_dict,
                              num_exp=10, custom_metric_dict=None)
-b.benchmark_sample_complexity(T_list=[100, 500, 1000], num_vars=5, graph_density=0.1,\
-                           data_max_lag=3,
-                           fn = lambda x:x, coef=0.1, noise_fn=np.random.randn) # default arguments in the library
-# note that the first argument for all the benchmarking methods is always the list of values of the variant
+
+
+# Obtain the times taken for each algorithm, at each number of variables
+times_per_vars = dict()
+for num_vars in [5, 10, 20, 50]:
+    b.benchmark_sample_complexity(T_list=[100, 500, 2000], num_vars=num_vars, graph_density=0.2,\
+                                data_max_lag=3,
+                                fn = lambda x:np.log(abs(x)) + np.sin(x), # Non-linearity
+                                coef=0.1, noise_fn=np.random.randn)
+    for algo in algo_dict.keys():
+        times_taken = [results[algo]['time_taken'] for results in b.results_full]
+        times_per_vars[algo] = np.mean(times_taken)
+
+
+with open('results_50vars.txt', 'w') as f:
+    f.write(str(b.results_full))
+    
+with open('times_per_vars.txt', 'w') as f:
+    f.write(str(times_per_vars))
 
 plt=b.plot('f1_score', xaxis_mode=1)
-# plt.savefig('myfig.pdf')
+plt.savefig('f1_score_50vars.pdf')
 plt.show()
 
 plt=b.plot('time_taken', xaxis_mode=1)
+plt.savefig('time_taken_50vars.pdf')
 plt.show()
