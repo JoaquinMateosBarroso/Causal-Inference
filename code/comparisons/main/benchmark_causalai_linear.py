c398c1ad67f40827b3a16fc29bb5762318c9934b
Joaquín Mateos Barroso <jmateosbarroso@gmail.com>
2025-01-31 09:43:01 +0100

    prepared 2 different benchmarks

--- code/comparisons/main/benchmark_causalai_linear.py
+++ code/comparisons/main/benchmark_causalai_linear.py
@@ -15,7 +15,7 @@ import numpy as np
 
 from algo_tigramite import Extractor_LPCMCI, Extractor_PCMCI, Extractor_FullCI, Extractor_DiscretizedPC
 
-folder = 'linear_results/'
+
 
 algo_dict = {
             'PCMCI': partial(Extractor_PCMCI),
@@ -45,37 +45,37 @@ times_per_vars = dict()
 for num_vars in [10, 20]:
     b.benchmark_sample_complexity(T_list=[100, 500, 2000, 5000], num_vars=num_vars, graph_density=0.2,\
                                 data_max_lag=3,
-                                fn = lambda x:np.log(abs(x)) + np.sin(x), # Non-linearity
+                                fn = lambda x:x,
                                 coef=0.1, noise_fn=np.random.randn)
     
     b.aggregate_results('time_taken')
     
     times_per_vars[num_vars] = {algo: np.mean(results) for algo, results in zip(algo_dict.keys(), b.results_mean)}
 
-    with open(f'{folder}results_{num_vars}vars.txt', 'w') as f:
-        f.write(str(b.results_full))
-
     print(f'Finished {num_vars} variables')
+
+    with open(f'results_{num_vars}vars.txt', 'w') as f:
+        f.write(str(b.results_full))
         
-with open(f'{folder}times_per_vars.txt', 'w') as f:
+with open('times_per_vars.txt', 'w') as f:
     f.write(str(times_per_vars))
 
 plt=b.plot('f1_score', xaxis_mode=1)
-plt.savefig(f'{folder}f1_score.pdf')
+plt.savefig('f1_score.pdf')
 plt.show()
 plt.clf()
 
 plt=b.plot('precision', xaxis_mode=1)
-plt.savefig(f'{folder}precision.pdf')
+plt.savefig('precision.pdf')
 plt.show()
 plt.clf()
 
 plt=b.plot('recall', xaxis_mode=1)
-plt.savefig(f'{folder}recall.pdf')
+plt.savefig('recall.pdf')
 plt.show()
 plt.clf()
 
 plt=b.plot('time_taken', xaxis_mode=1)
-plt.savefig(f'{folder}time_taken.pdf')
+plt.savefig('time_taken.pdf')
 plt.show()
 plt.clf()
