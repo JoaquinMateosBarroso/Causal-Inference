472f79f047728e84a46c9f10f7d59e05c99d4bf0
Joaquín Mateos Barroso <jmateosbarroso@gmail.com>
2025-03-28 10:56:47 +0100

    update of ts summary graphs

--- DocumentAuxiliars/Graphs/tigramite/plot_ts_graphs.py
+++ DocumentAuxiliars/Graphs/tigramite/plot_ts_graphs.py
@@ -1,9 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tigramite.data_processing import DataFrame
-from tigramite.pcmci import PCMCI
-from tigramite.independence_tests.parcorr import ParCorr
-from tigramite.plotting import plot_time_series_graph
+# from tigramite.plotting import plot_time_series_graph
+from tigramite_plotting_modified import plot_time_series_graph
 
 
 ts_basic_window_graph = np.array([
@@ -24,13 +22,13 @@ ts_basic_summary_graph = np.array([
 
 ts_basic_fully_connected_graph = np.ones((2, 2, 4))
 
-def plot_graph(graph):
-    plot_time_series_graph(graph=graph,
+def plot_graph(graph, summary=False):
+    plot_time_series_graph(graph=graph, summary=summary,
             var_names=[f'$X^{i}$' for i in range(1, graph.shape[0]+1)],
             figsize=(5, 5))
 
 
 if __name__ == '__main__':
-    plot_graph(ts_basic_fully_connected_graph)
-    plt.savefig('figs/' + 'ts_basic_fully_connected_graph.pdf')
+    plot_graph(ts_basic_extended_summary_graph, summary=True)
+    plt.savefig('figs/' + 'ts_basic_extended_summary_graph.pdf')
 
